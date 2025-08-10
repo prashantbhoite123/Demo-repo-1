a2ac83b2c9deab08b06ccce71254e8d1f6a12613
Prashant-penteai <prashant@penteai.com>
2025-08-10 17:55:04 +0530

    add my learning msg

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       hello prashant
+
+      <h2>Hey iam prashant and iam learning git in dept </h2>
     </div>
   );
 }
