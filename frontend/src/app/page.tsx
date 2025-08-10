0f33921e0910db09f323ebdd9f8507e01f63c343
Prashant-penteai <prashant@penteai.com>
2025-08-10 18:17:09 +0530

    remove age line

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
       hello prashant
 
       <h2>Hey iam prashant and iam learning git in dept </h2>
+      <h3>hey its me prashant and iam a fullstack engineer</h3>
+      
     </div>
   );
 }
