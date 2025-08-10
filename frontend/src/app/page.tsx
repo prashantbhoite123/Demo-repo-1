a7ae984f6c165a21dfe946a553bc26da45323a32
Prashant-penteai <prashant@penteai.com>
2025-08-10 18:55:54 +0530

    learn complete git hub which industry level use

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,6 +7,11 @@ export default function Home() {
       </div>
       <h2>Hey iam prashant and iam learning git in dept </h2>
       <h3>hey its me prashant and iam a fullstack engineer</h3>
+
+      <div>
+        <p>Welcome to my page!</p>
+        <p>Feel free to explore and learn more about my projects.</p>
+  </div>
     </div>
   )
 }
