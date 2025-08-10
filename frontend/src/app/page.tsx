6ae20888473a96f40319eba480d28513c5e50a45
Prashant-penteai <prashant@penteai.com>
2025-08-10 18:32:58 +0530

    Revert "remove age line"
    This reverts commit 0f33921e0910db09f323ebdd9f8507e01f63c343.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,9 @@
-
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       hello prashant
-
       <h2>Hey iam prashant and iam learning git in dept </h2>
       <h3>hey its me prashant and iam a fullstack engineer</h3>
-      <head>hello there iam trying git revert</head>
     </div>
-  );
+  )
 }
