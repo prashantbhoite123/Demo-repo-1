26f67d2ca904eac6c99264afc9fb76166a7116a9
Prashant-penteai <prashant@penteai.com>
2025-08-10 18:40:24 +0530

    save a day of boss

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       hello prashant
+      <div>
+        <h2>fixed a bug which it make production broke</h2>
+      </div>
       <h2>Hey iam prashant and iam learning git in dept </h2>
       <h3>hey its me prashant and iam a fullstack engineer</h3>
     </div>
